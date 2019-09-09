2b7c61d31c72f54e0cd5452033409761a4f13db6
rezan21 <reza.naghshineh@gmail.com>
2019-09-09 12:40:03 +0000

    Updates

---
+++ precache-manifest.487391e64fa795dce03bc6a20afee509.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "046db08b39fe52ff7213c0f1acb053b2",
+    "url": "/Flight-Explorer/index.html"
+  },
+  {
+    "revision": "a4060101772062ceef24",
+    "url": "/Flight-Explorer/static/css/main.af2c559e.chunk.css"
+  },
+  {
+    "revision": "f25574e713d1521dbde2",
+    "url": "/Flight-Explorer/static/js/2.538f7ae5.chunk.js"
+  },
+  {
+    "revision": "a4060101772062ceef24",
+    "url": "/Flight-Explorer/static/js/main.d9f80289.chunk.js"
+  },
+  {
+    "revision": "ca03abf2248b7c4899a3",
+    "url": "/Flight-Explorer/static/js/runtime~main.ca9466f6.js"
+  },
+  {
+    "revision": "068b1c3456f23d56ea00acdb6893004b",
+    "url": "/Flight-Explorer/static/media/bg.068b1c34.svg"
+  },
+  {
+    "revision": "57fa854e33d73bdf6d142841b75b5b43",
+    "url": "/Flight-Explorer/static/media/pattern.57fa854e.png"
+  },
+  {
+    "revision": "a39c93d8958a25246466d2b2bab76162",
+    "url": "/Flight-Explorer/static/media/plane.a39c93d8.png"
+  },
+  {
+    "revision": "cb92d04f3f578a73453df2004fa915f8",
+    "url": "/Flight-Explorer/static/media/spinner.cb92d04f.gif"
+  }
+]);
